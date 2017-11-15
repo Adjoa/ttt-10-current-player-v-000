b33ce717c2e8d922cb6c868d5e2708d53c2142cd
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-15 18:15:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,5 +8,11 @@ def turn_count(board)
   return counter
 end
 
-# def current_player
-# end
+def current_player(board)
+  turns = turn_count(board)
+  if turns%2 == 0
+    return 'X'
+  else
+    return 'O'
+  end
+end
